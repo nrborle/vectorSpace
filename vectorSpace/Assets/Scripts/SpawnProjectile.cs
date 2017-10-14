73fadd273c55a40e1d9abd799058eed4a9765c77
NickB <nickb.ubc@gmail.com>
2017-10-14 16:19:27 -0700

    Add Porject to Repository
    This is the first stable and working version of Vector Space.
    
    Version 1.0.0

---
+++ vectorSpace/Assets/Scripts/SpawnProjectile.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnProjectile : MonoBehaviour {
+	public GameObject Projectile;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) {
+			var mousePos = Input.mousePosition;
+			Debug.Log (mousePos.y);
+			if ((mousePos.y > 60)) {
+				mousePos.z = 0.0f;       
+				var objectPos = Camera.main.ScreenToWorldPoint (mousePos);
+				Instantiate (Projectile, objectPos, Quaternion.identity);
+			}
+		}
+	}
+} 
---
+++ vectorSpace/Assets/Scripts/TextGetY.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TextGetY : MonoBehaviour {
+	public static int yInt;
+	// Use this for initialization
+	void Start () {
+		var input = gameObject.GetComponent<InputField>();
+		var se= new InputField.SubmitEvent();
+		se.AddListener(SubmitName);
+		input.onEndEdit = se;
+
+		//or simply use the line below, 
+		//input.onEndEdit.AddListener(SubmitName);  // This also works
+	}
+
+	private void SubmitName(string arg0){
+		//Debug.Log("Y: " + arg0);
+		yInt = Int32.Parse(arg0);
+	}
+
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}

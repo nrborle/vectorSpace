5c53a37ea005df7fb00bac218249920c642ab69c
NickB <nickb.ubc@gmail.com>
2017-11-24 13:25:42 -0800

    Continued work on Sandbox mode
    Has a drop down menu but it needs to change the prefab to spawn and create the spawning script. also, implement delete function.

---
+++ Assets/Scripts/showText.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class showText : MonoBehaviour {
+
+	public Text textShow = null;
+
+	public void updateText(string word){
+		textShow.text = word;
+	}
+}

29028b5bbf74e83d6276fc92e0288627a89228b1
NickB <nickb.ubc@gmail.com>
2017-11-26 15:51:33 -0800

    Updated to Version 1.3.5
    First working version of sandbox mode is now complete. There's probably lots of bugs to do with this.

--- Assets/Scripts/sandboxController.cs
+++ Assets/Scripts/sandboxController.cs
@@ -1,12 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sandboxController : MonoBehaviour {
 
 	public float xScale;
 	public float yScale;
-	public GameObject Prefab;
+	public GameObject PrefabStar;
+	public GameObject PrefabPlanet;
+	public Text selectionText = null;
 	int xValue = TextGetX.xInt;
 	int yValue = TextGetY.yInt;
 	public Vector2 velocityVector;
@@ -15,7 +18,7 @@ public class sandboxController : MonoBehaviour {
 	void Start () {
 		velocityVector.x = xValue;
 		velocityVector.y = yValue;
-		gameObject.GetComponent<Rigidbody2D> ().velocity = velocityVector;
+		//gameObject.GetComponent<Rigidbody2D> ().velocity = velocityVector;
 	}
 	
 	// Update is called once per frame
@@ -26,17 +29,42 @@ public class sandboxController : MonoBehaviour {
 			if ((mousePos.y > 60)) {
 				mousePos.z = 0.0f;       
 				var objectPos = Camera.main.ScreenToWorldPoint (mousePos);
-				if (Prefab.name == "Projectile") {
-					var projectile = Instantiate (Prefab, objectPos, Quaternion.identity);
+				if (selectionText.text == "Planet") {
+					//Planet
+					var projectile = Instantiate (PrefabPlanet, objectPos, Quaternion.identity);
 					projectile.transform.localScale = new Vector3 (xScale, yScale, 1.0f);
 					projectile.GetComponent<Rigidbody2D> ().velocity = velocityVector;
-				}else{
-					var projectile = Instantiate (Prefab, objectPos, Quaternion.identity);
+					Debug.Log(selectionText.text);
+				} else if (selectionText.text == "Star") {
+					//Star
+					var projectile = Instantiate (PrefabStar, objectPos, Quaternion.identity);
 					projectile.transform.localScale = new Vector3 (xScale, yScale, 1.0f);
 					projectile.GetComponent<Rigidbody2D> ().velocity = velocityVector;
-					
-			}
+					Debug.Log(selectionText.text);
+				} else if (selectionText.text == "Delete") {
+					//Deleting
+					Debug.Log(selectionText.text);
+					DestroyAllGameObjects ();
+				} else if (selectionText.text == null) {
+					//Null
+					Debug.Log(selectionText.text);
+				} else {
+					//Not Null and not planet, star, or delete!!!
+					Debug.Log(selectionText.text);
+				}
 		}
 	}
 }
+
+	public void DestroyAllGameObjects(){
+		var planets = GameObject.FindGameObjectsWithTag ("Projectile");
+		var stars = GameObject.FindGameObjectsWithTag ("Gravity");
+		foreach (var planet in planets){
+			Destroy(planet);
+		}
+		foreach (var star in stars){
+			Destroy(star);
+		}
+	}
+
 }

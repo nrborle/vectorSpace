6c6e610d9d417a19e04f97dda51e306124393092
Nick Borle <nickb.ubc@gmail.com>
2017-11-26 13:10:54 -0800

    Continuing work on Sandbox mode

--- Assets/Scripts/sandboxController.cs
+++ Assets/Scripts/sandboxController.cs
@@ -2,17 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnProjectile : MonoBehaviour {
+public class sandboxController : MonoBehaviour {
 
 	public float xScale;
 	public float yScale;
-
 	public GameObject Prefab;
+	int xValue = TextGetX.xInt;
+	int yValue = TextGetY.yInt;
+	public Vector2 velocityVector;
+
 	// Use this for initialization
 	void Start () {
-
+		velocityVector.x = xValue;
+		velocityVector.y = yValue;
+		gameObject.GetComponent<Rigidbody2D> ().velocity = velocityVector;
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire1") && (Input.mousePosition.y > Screen.height*0.0575) && (Input.mousePosition.y < Screen.height*0.825)) {
@@ -24,8 +29,14 @@ public class SpawnProjectile : MonoBehaviour {
 				if (Prefab.name == "Projectile") {
 					var projectile = Instantiate (Prefab, objectPos, Quaternion.identity);
 					projectile.transform.localScale = new Vector3 (xScale, yScale, 1.0f);
-				}
+					projectile.GetComponent<Rigidbody2D> ().velocity = velocityVector;
+				}else{
+					var projectile = Instantiate (Prefab, objectPos, Quaternion.identity);
+					projectile.transform.localScale = new Vector3 (xScale, yScale, 1.0f);
+					projectile.GetComponent<Rigidbody2D> ().velocity = velocityVector;
+					
 			}
 		}
 	}
-} 
+}
+}

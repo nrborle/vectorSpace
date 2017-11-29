71e877e4f1131ea3bd1f4776c35f0180c82d85c8
NickB <nickb.ubc@gmail.com>
2017-11-29 12:20:29 -0800

    Started work on camera zoom
    Want to be able to zoom camera in and out  for sandbox mode.

---
+++ Assets/Scripts/cameraZoom.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraZoom : MonoBehaviour {
+
+	public float zoomSpeed = 300;
+	void Update()
+	{
+		var mouseScroll = Input.GetAxis("Mouse ScrollWheel");
+
+		if (mouseScroll!=0)
+		{
+			transform.Translate(transform.forward * mouseScroll * zoomSpeed * Time.deltaTime, Space.Self);
+		}
+	}
+}
